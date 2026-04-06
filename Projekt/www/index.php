e58b4528ace9b2dc8b4a03634abb96188c7ffcf1
romancieciuch <roman@r1media.pl>
2026-04-06 09:22:27 +0200

    Sesja ważna 7 dni

--- Projekt/www/index.php
+++ Projekt/www/index.php
@@ -1,6 +1,14 @@
 <?php
 
 declare(strict_types=1);
+ini_set('session.gc_maxlifetime', 604800);
+session_set_cookie_params([
+	'lifetime' => 604800,
+	'path' => '/',
+	'secure' => true,
+	'httponly' => true,
+	'samesite' => 'Lax'
+]);
 session_start();
 ob_start();
 require_once $_SERVER["DOCUMENT_ROOT"] . "/../app/init.php";
