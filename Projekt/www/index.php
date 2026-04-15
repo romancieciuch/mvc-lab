d6af739ac91d026ec5545381e1d9fa0a5ae17284
romancieciuch <roman@r1media.pl>
2026-04-15 10:50:14 +0200

    Ciasteczko sesyjne

--- Projekt/www/index.php
+++ Projekt/www/index.php
@@ -2,13 +2,16 @@
 
 declare(strict_types=1);
 ini_set('session.gc_maxlifetime', 604800);
+
 session_set_cookie_params([
-	'lifetime' => 604800,
-	'path' => '/',
-	'secure' => true,
-	'httponly' => true,
-	'samesite' => 'Lax'
+    'lifetime' => 604800,
+    'path'     => '/',
+    'domain'   => '',
+    'secure'   => true,
+    'httponly' => true,
+    'samesite' => 'Strict'
 ]);
+
 session_start();
 ob_start();
 require_once $_SERVER["DOCUMENT_ROOT"] . "/../app/init.php";
